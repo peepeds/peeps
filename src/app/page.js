bcad757be116b392d5308b3fbb46f9f6a82503a3
peepeds <edbertmac@gmail.com>
2024-10-05 00:41:10 +0700

    fix: bug links

--- src/app/page.js
+++ src/app/page.js
@@ -2,16 +2,14 @@ import HeroSection from "./components/HeroSection";
 import AboutSection from "./components/AboutSection";
 import { StarsBackground } from "./components/ui/stars-background";
 
-
-
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col bg-[#121212]">
-      <div className="container px-12 py-4 mx-auto mt-24">
-        <StarsBackground starDensity={0.00025} allStarsTwinkle='false' />
+    <main className="flex min-h-screen flex-col bg-[#121212] relative">
+      <StarsBackground starDensity={0.00045} allStarsTwinkle={false} />
+      <div className="container px-12 py-4 mx-auto mt-24 z-10 relative">
         <HeroSection />
         <AboutSection />
-        
+
       </div>
     </main>
   );
